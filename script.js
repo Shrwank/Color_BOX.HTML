19233a23ff209d7ba368e82d77f15c0a9b22ea46
Shrwan Kumar <156888806+Shrwank@users.noreply.github.com>
2024-04-04 19:39:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+let coloring = document.getElementById('main');
+
+function red() {
+  coloring.style.backgroundColor = 'red';
+}
+
+function green() {
+  coloring.style.backgroundColor = 'green';
+}
+
+function blue() {
+  coloring.style.backgroundColor = 'blue';
+}
+
+function yellow() {
+  coloring.style.backgroundColor = 'yellow';
+}
+
+function purple() {
+  coloring.style.backgroundColor = 'purple';
+}
+
+function orange() {
+  coloring.style.backgroundColor = 'orange';
+}
+
+function pink() {
+  coloring.style.backgroundColor = 'pink';
+}
+
+function cyan() {
+  coloring.style.backgroundColor = 'cyan';
+}
